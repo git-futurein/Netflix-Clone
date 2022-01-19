7a34b5d8e2a7c3f75295750a2ed9cebcec5eb554
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-01-19 17:36:49 +0600

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,51 @@
 $(function () {
   'use strict';
-
-
   // WoW Js
-  var wow = new WOW(
-    {
-      offset: 2,
-      mobile: false
-    }
-  )
+  var wow = new WOW({
+    offset: 2,
+    mobile: false
+  })
   wow.init();
+
+
+
+  // $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav'
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   dots: true,
+  // });
+
+
+  // banner slide
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipe: true,
+    fade: false,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    swipe: false,
+    centerMode: false,
+    focusOnSelect: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"> <i class="fas fa-chevron-right"></i></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-chevron-left"></i></button>'
+  });
+
+
+
 
 });
